8d4757098a6c70aa2efab996156858e332e2fd4f
KeddyB <bokelusi@gmail.com>
2023-11-23 12:02:56 +0100

    home page

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,16 @@ const home = () => {
   return (
     <>
       Hero banner
+      
       <div>
         <h2>Best selling products</h2>
         <p>speakers of many variation</p>
       </div>
+      <div>
+        {['Product 1','Product 2','Product 3'].map((product) => product)}
+      </div>
+
+      Footer
     </>
   )
 }
