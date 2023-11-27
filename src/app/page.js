5f009db2fca1798644296ccd9d5f9622b7350f05
KeddyB <bokelusi@gmail.com>
2023-11-27 11:59:57 +0100

    second commit of the day from one error to another

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,12 @@ import './globals.css'
 import { Product, Footer, HeroBanner} from '../../components/index'
 import { sanityClient } from '../../lib/client'
 
-const home = ({ products, bannerData}) => {
+export const home = async ({ products, bannerData}) => {
+  const data = await fetchData()
   
   return (
     <div>
-      <HeroBanner />
-      {console.log(bannerData)}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className='products-heading'>
         <h2>Best selling products</h2>
         <p>speakers of many variation</p>
@@ -22,15 +22,19 @@ const home = ({ products, bannerData}) => {
   )
 }
 
-export const fetchProducts = async () => {
+export const fetchData = async () => {
   const query = '*[_type == "product"]'
-  const products = await client.fetch(query)
+  const products = await sanityClient.fetch(query)
 
   const bannerQuery = '*[_type == "banner"]'
-  const bannerData = await client.fetch(bannerQuery)
+  const bannerData = await sanityClient.fetch(bannerQuery)
+
+  // if (!products.ok || !bannerData.ok){
+  //   throw new Error('failed to fetch data')
+  // }
 
   return {
-    props: { products, bannerData }
+    props: {products, bannerData}
   }
 }
 
