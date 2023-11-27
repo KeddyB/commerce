6c3c4cf64f22b4bfdc4adcc18aab3963595a9be6
KeddyB <bokelusi@gmail.com>
2023-11-27 11:19:49 +0100

    getServerSideProps not supported after nextjs 12

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import './globals.css'
 import { Product, Footer, HeroBanner} from '../../components/index'
-import { client } from '../../lib/client'
+import { sanityClient } from '../../lib/client'
 
 const home = ({ products, bannerData}) => {
+  
   return (
     <div>
       <HeroBanner />
-      
+      {console.log(bannerData)}
       <div className='products-heading'>
         <h2>Best selling products</h2>
         <p>speakers of many variation</p>
       </div>
       <div className='products-container'>
-        {['product 1', 'products 2'].map((product) => product.name)}
+        {products?.map((product) => product.name)}
       </div>
 
       <Footer />
@@ -21,16 +22,16 @@ const home = ({ products, bannerData}) => {
   )
 }
 
-// export const getServerSideProps = async () => {
-//   const query = '*[_type == "product"]'
-//   const products = await client.fetch(query)
+export const fetchProducts = async () => {
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
 
-//   const bannerQuery = '*[_type == "banner"]'
-//   const bannerData = await client.fetch(bannerQuery)
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
 
-//   return {
-//     props: { products, bannerData }
-//   }
-// }
+  return {
+    props: { products, bannerData }
+  }
+}
 
 export default home
