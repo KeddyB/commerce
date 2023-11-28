dedad0c710214f97a5ecc66d07f8362bb7925971
KeddyB <bokelusi@gmail.com>
2023-11-28 13:24:56 +0100

    i lied, work day only just began. more commits otw

--- src/app/page.js
+++ src/app/page.js
@@ -3,28 +3,13 @@ import './globals.css'
 import { Product, Footer, HeroBanner} from '../../components/index'
 import { sanityClient } from '../../lib/client'
 
-
-export const fetchData = async () => {
-  const query = '*[_type == "product"]'
-  const products = await sanityClient.fetch(query)
-
-  const bannerQuery = '*[_type == "banner"]'
-  const bannerData = await sanityClient.fetch(bannerQuery)
-
-  // if (!products.ok || !bannerData.ok){
-  //   throw new Error('Failed to fetch data')
-  // }
-
-  return {products, bannerData}
-}
-
-export default async function home ({ products, bannerData}) {
+export const home = async ({ products, bannerData}) => {
   const data = await fetchData()
   
   return (
     <div>
-      <HeroBanner /*heroBanner={bannerData.length && bannerData[0]}*/ />
-      {console.log(bannerData)} 
+      <HeroBanner /* heroBanner={bannerData.length && bannerData[0]} */ />
+      {console.log(bannerData)}
       <div className='products-heading'>
         <h2>Best selling products</h2>
         <p>speakers of many variation</p>
@@ -37,3 +22,19 @@ export default async function home ({ products, bannerData}) {
     </div>
   )
 }
+
+export const fetchData = async () => {
+  const query = await fetch ['*[_type == "product"]']
+  const products = await sanityClient.fetch(query)
+
+  const bannerQuery = await fetch ['*[_type == "banner"]']
+  const bannerData = await sanityClient.fetch(bannerQuery)
+
+  // if (!products.ok || !bannerData.ok){
+  //   throw new Error('Failed to fetch data')
+  // }
+
+  return {products, bannerData}
+}
+
+export default home
