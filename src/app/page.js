afa19a30428f81765efcc503facb1f627b76f1bc
KeddyB <bokelusi@gmail.com>
2023-11-29 10:22:47 +0100

    i am so close to fixing the bug

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './globals.css'
 import { Product, Footer, HeroBanner} from '../../components/index'
-import { sanityClient } from '../../lib/client'
+import { client } from '../../lib/client'
+import { groq } from "next-sanity"
 
-export const home = async ({ products, bannerData}) => {
+export const home = async ({products, bannerData}) => {
   const data = await fetchData()
   
   return (
     <div>
-      <HeroBanner /* heroBanner={bannerData.length && bannerData[0]} */ />
-      {console.log(bannerData)}
+      <HeroBanner /* heroBanner={bannerData?.length && bannerData[0]}*/ />
+      
       <div className='products-heading'>
         <h2>Best selling products</h2>
         <p>speakers of many variation</p>
@@ -24,17 +25,27 @@ export const home = async ({ products, bannerData}) => {
 }
 
 export const fetchData = async () => {
-  const query = await fetch ['*[_type == "product"]']
-  const products = await sanityClient.fetch(query)
+  const query = groq`*[_type == "product"]`
+  const products = await client.fetch(query)
 
-  const bannerQuery = await fetch ['*[_type == "banner"]']
-  const bannerData = await sanityClient.fetch(bannerQuery)
+  const bannerQuery = groq`*[_type == "banner"]`
+  const bannerData = await client.fetch(bannerQuery)
 
   // if (!products.ok || !bannerData.ok){
   //   throw new Error('Failed to fetch data')
   // }
 
-  return {products, bannerData}
+  return {
+    props:{products, bannerData}
+  }
+//   const query = client.fetch(groq`
+//   {
+//     'products': *[_type == "product"],
+//     'bannerData': ['*[_type == "banner"]'
+//   }
+// `)
+
+//   return query
 }
 
 export default home
