c92dbeb176c7ef72f77fb6709838c4a4c928d453
KeddyB <bokelusi@gmail.com>
2023-12-05 16:05:10 +0100

    so i need to learn more about getstaticpaths

--- src/app/page.js
+++ src/app/page.js
@@ -30,12 +30,12 @@ export const home = async () => {
 }
 
 export const fetchProduct = async () => {
-  const query = client.fetch('*[_type == "product"]')
+  const query = client.fetch(`*[_type == "product"]`)
   return query
 }
 
 export const fetchBanner = async () => {
-  const bquery = client.fetch('*[_type == "banner"]')
+  const bquery = client.fetch(`*[_type == "banner"]`)
   return bquery
 }
 
--- src/app/product/[[...index]]/page.jsx
+++ src/app/product/[[...index]]/page.jsx
@@ -2,27 +2,25 @@ import React from "react"
 import { client, urlFor } from "../../../../lib/client"
 
 
-export const fetchProduct = async ({ params:{ slug } }) => {
-  const product = client.fetch(`*[_type == "product" && slug.current == '${slug}][0]`)
+export const fetchProduct = async ({ params: {slug} }) => {
+  const product = await client.fetch(`*[_type == "product" && slug.current == '${slug}][0]`)
   return product
 }
 
 export const fetchProducts = async () => {
-  const productsQuery =  client.fetch('*[_type == "product"]')
-  return productsQuery
+  const products =  client.fetch('*[_type == "product"]')
+  return products
 }
 
-export default async function ProductDetails() {
-  const productData = await fetchProduct()
-  const product = await fetchProducts()
+export default async function ProductDetails({ product, products }) {
 
-
+  const { image, name, details, price} = product
   return(
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src='' />
+            <img src={urlFor(image && image[0])} />
           </div>
         </div>
       </div>
