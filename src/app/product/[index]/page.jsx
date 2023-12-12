53fe9082e704390f7ff6c654fe1f43f9d16e4746
KeddyB <bokelusi@gmail.com>
2023-12-12 15:08:55 +0100

    that was not the final commit but it sure had a light shined on the issue here

---
+++ src/app/product/[index]/page.jsx
@@ -0,0 +1,56 @@
+import React from "react"
+import { urlFor, client } from "../../../../lib/sanity"
+import { getProject } from "../../../../lib/utils"
+
+export default async function ProductDetails({ params }) {
+  const slug = params.index
+  const index = await getProject(slug)
+
+  return(
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            {index.name}
+            {/* <img src={urlFor(image && image[0]).url()} /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+// export const generateStaticParams = async () => {
+//   const query = `*[_type == "product"] {
+//     slug {
+//       current
+//     }
+//   }
+//   `;
+
+//   const products = await client.fetch(query).then((res) => res.json());
+
+//   const paths = products.map((product) => ({
+//     params: { 
+//       slug: product.slug.current
+//     }
+//   }));
+
+//   return {
+//     paths,
+//     fallback: 'blocking'
+//   }
+// }
+
+// export const generateProps = async ({ params: { slug }}) => {
+//   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+//   const productsQuery = '*[_type == "product"]'
+  
+//   const product = await client.fetch(query);
+//   const products = await client.fetch(productsQuery);
+
+//   console.log(product);
+
+//   return {
+//     props: { products, product }
+//   }
+// }
