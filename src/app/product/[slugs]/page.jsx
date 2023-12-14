d1fcfbf70e9835d466338313bfac940e9bf72ea8
KeddyB <bokelusi@gmail.com>
2023-12-14 10:06:14 +0100

    there are lots of ways to solve problems

--- src/app/product/[slugs]/page.jsx
+++ src/app/product/[slugs]/page.jsx
@@ -1,15 +1,14 @@
 import React from "react"
 import { urlFor, client } from "../../../../lib/sanity"
-import { getProject } from "../../../../lib/utils"
+import { fetchProduct, getProject } from "../../../../lib/utils"
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from "react-icons/ai"
+import { Product } from "../../../../components"
 
-type Props = {
-  params: { slugs: string }
-}
-
-export default async function ProductDetails({ params }: Props) {
+export default async function ProductDetails({ params }) {
   const slug = params.slugs
   const slugs = await getProject(slug)
+
+  const products = await fetchProduct()
 
   return(
     <div>
@@ -24,7 +23,7 @@ export default async function ProductDetails({ params }: Props) {
             />
           </div>
           <div className="small-images-container">
-            {slugs.image?.map((item, i) =>(
+            {slugs.image.map((item, i) =>(
               <img src={urlFor(item).url()}
                 width={100}
                 height={100}
@@ -33,6 +32,7 @@ export default async function ProductDetails({ params }: Props) {
                 // onMouseEnter=""
               />
             ))}
+            
           </div>
         </div>
         <div className="product-detail-desc">
@@ -60,6 +60,20 @@ export default async function ProductDetails({ params }: Props) {
                 <span className="plus"><AiOutlinePlus /></span>
               </p>
             </div>
+            <div className="buttons">
+              <button type="button" className="add-to-cart" onClick="">Add to cart</button>
+              <button type="button" className="buy-now" onClick="">Buy now</button>
+            </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product = {item}/>
+            ))}
+          </div>
         </div>
       </div>
     </div>
