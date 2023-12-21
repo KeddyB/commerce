8c0856df5c31d24275f20b7aa476fba4cae5148a
KeddyB <bokelusi@gmail.com>
2023-12-21 14:21:50 +0100

    i will come back to this later... the errors are some much and they are conflicting

--- src/app/product/[slugs]/page.jsx
+++ src/app/product/[slugs]/page.jsx
@@ -1,112 +1,99 @@
-"use client";
 import React from "react";
 import { urlFor, client } from "../../../../lib/sanity";
 import { fetchProduct, getProject } from "../../../../lib/utils";
-import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar} from "react-icons/ai";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
 import { Product } from "../../../../components";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
-const queryClient = new QueryClient();
-
-export default function ProductDetails({ params }) {
-  const { isLoading, error, slugs } = useQuery("slugs", async (slug) => {
-    return client.fetch(`*[_type == "product" && slug.current == '${slug}'][0]`,{ slug });
-  });
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-  console.log(slugs)
+export default async function ProductDetails({ params }) {
   const slug = params.slugs;
-  // const slugs = await getProject(slug)
-  // const products = await fetchProduct()
-  //const { decQty, incQty, qty} = useStateContext()
+  const slugs = await getProject(slug);
+  const products = await fetchProduct();
   let index = 0;
   function setIndex(a) {
     return (index = a);
   }
   return (
-    <QueryClientProvider client={queryClient}>
-      <div>
-        <div className="product-detail-container">
-          <div>
-            <div className="image-container">
-              <img
-                src={urlFor(slugs.image && slugs.image[index]).url()}
-                width={400}
-                height={400}
-                key={slug._id}
-                alt=""
-              />
-            </div>
-            <div className="small-images-container">
-              {slugs.image.map((item, i) => (
-                <img
-                  src={urlFor(item).url()}
-                  width={100}
-                  height={100}
-                  alt=""
-                  className={
-                    i === index ? "small-image selected-image" : "small-image"
-                  }
-                  // onMouseEnter = {() => {
-                  //   setIndex(i);
-                  // }}
-                />
-              ))}
-            </div>
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img
+              src={urlFor(slugs.image && slugs.image[index]).url()}
+              width={400}
+              height={400}
+              key={slug._id}
+              alt=""
+            />
           </div>
-          <div className="product-detail-desc">
-            <h1>{slugs.name}</h1>
-            <div className="reviews">
-              <div>
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiOutlineStar />
-              </div>
-              <p>(20)</p>
-            </div>
-            <h4>Details: </h4>
-            <p>{slugs.details}</p>
-            <p className="price">${slugs.price}</p>
-            <div className="quantity">
-              <h3>Quantity: </h3>
-              <p className="quantity-desc">
-                <span className="minus" /*onClick={decQty}*/>
-                  <AiOutlineMinus />
-                </span>
-                <span className="num">{/* {qty} */}0</span>
-                <span className="plus" /*onClick={incQty}*/>
-                  <AiOutlinePlus />
-                </span>
-              </p>
-            </div>
-            <div className="buttons">
-              <button type="button" className="add-to-cart" onClick="">
-                Add to cart
-              </button>
-              <button type="button" className="buy-now" onClick="">
-                Buy now
-              </button>
-            </div>
+          <div className="small-images-container">
+            {slugs.image.map((item, i) => (
+              <img
+                src={urlFor(item).url()}
+                width={100}
+                height={100}
+                alt=""
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                // onMouseEnter = {() => {
+                //   setIndex(i);
+                // }}
+              />
+            ))}
           </div>
         </div>
-        <div className="maylike-products-wrapper">
-          <h2>You may also like</h2>
-          <div className="marquee">
-            <div className="maylike-products-container track">
-              {products.map((item) => (
-                <Product key={item._id} product={item} />
-              ))}
+        <div className="product-detail-desc">
+          <h1>{slugs.name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
             </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details: </h4>
+          <p>{slugs.details}</p>
+          <p className="price">${slugs.price}</p>
+          <div className="quantity">
+            <h3>Quantity: </h3>
+            <p className="quantity-desc">
+              <span className="minus" /*onClick={decQty}*/>
+                <AiOutlineMinus />
+              </span>
+              <span className="num">{/* {qty} */}0</span>
+              <span className="plus" /*onClick={incQty}*/>
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onClick="">
+              Add to cart
+            </button>
+            <button type="button" className="buy-now" onClick="">
+              Buy now
+            </button>
           </div>
         </div>
       </div>
-    </QueryClientProvider>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
